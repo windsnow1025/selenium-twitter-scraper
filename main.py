00c20e1df7aeb985607c258c2e56b89d03717285
windsnow1025 <windsnow1025@gmail.com>
2024-01-25 15:36:24 +0000

    fix query

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from scraper.__main__ import main
 
-main(['--query=@SenatorBaldwin until:2023-06-09 since:2023-03-01', '--tweets=500'])
+main(['--query=(from:@SenatorBaldwin) until:2023-06-09 since:2023-03-01', '--tweets=9999'])
