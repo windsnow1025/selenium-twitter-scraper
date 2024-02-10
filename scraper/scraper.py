06f29b4c9ebe09ced8a89308a0a55374e2901671
windsnow1025 <windsnow1025@gmail.com>
2024-02-10 15:06:57 +0000

    remove main exceptions

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -12,32 +12,24 @@ def main(
         latest: bool = False,
         top: bool = False,
 ):
-    try:
 
-        additional_data: list = add.split(",")
+    additional_data: list = add.split(",")
 
-        if not (username or hashtag or query):
-            print("Please specify only one of --username, --hashtag, or --query.")
-            sys.exit(1)
-
-        if latest and top:
-            print("Please specify either --latest or --top. Not both.")
-            sys.exit(1)
-
-        scraper.scrape_tweets(
-            max_tweets=tweets,
-            scrape_username=username,
-            scrape_hashtag=hashtag,
-            scrape_query=query,
-            scrape_latest=latest,
-            scrape_top=top,
-            scrape_poster_details="pd" in additional_data,
-        )
-        scraper.save_to_csv()
-
-    except KeyboardInterrupt:
-        print("\nScript Interrupted by user. Exiting...")
+    if not (username or hashtag or query):
+        print("Please specify only one of --username, --hashtag, or --query.")
         sys.exit(1)
-    except Exception as e:
-        print(f"Error: {e}")
+
+    if latest and top:
+        print("Please specify either --latest or --top. Not both.")
         sys.exit(1)
+
+    scraper.scrape_tweets(
+        max_tweets=tweets,
+        scrape_username=username,
+        scrape_hashtag=hashtag,
+        scrape_query=query,
+        scrape_latest=latest,
+        scrape_top=top,
+        scrape_poster_details="pd" in additional_data,
+    )
+    scraper.save_to_csv()
